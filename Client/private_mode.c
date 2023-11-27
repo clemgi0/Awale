325bfb91cf5922fe58734c1d245f9e5be53faed3
LordVERTON <daniel.verton@insa-lyon.fr>
2023-11-27 11:20:59 +0100

    ajout du chat + bio + savegrde

--- Client/private_mode.c
+++ Client/private_mode.c
@@ -1,6 +1,55 @@
 // Fonctions pour le mode privé.
 // Gestion de la liste d'amis pour les observateurs.
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "private_mode.h"
+
 typedef struct {
-    // Structure de données pour gérer le mode privé
-} PrivateModeData;
+    char friends[MAX_CLIENTS][MAX_USERNAME_LENGTH];
+    int numFriends;
+    // Autres informations liées au mode privé
+} PrivateModeInfo;
+
+static PrivateModeInfo privateModeData[MAX_CLIENTS];
+
+void addFriend(const char *username, const char *friendUsername) {
+    // Trouver l'utilisateur dans la liste des clients
+    for (int i = 0; i < numClients; ++i) {
+        if (strcmp(clients[i].username, username) == 0) {
+            // Ajouter un ami à la liste
+            if (privateModeData[i].numFriends < MAX_CLIENTS) {
+                strcpy(privateModeData[i].friends[privateModeData[i].numFriends], friendUsername);
+                privateModeData[i].numFriends++;
+            } else {
+                // Gérer l'erreur si la liste d'amis est pleine
+                printf("Error: Maximum friends reached\n");
+            }
+            return;
+        }
+    }
+
+    // Gérer l'erreur si l'utilisateur n'est pas trouvé
+    printf("Error: User not found\n");
+}
+
+int isFriend(const char *username, const char *potentialFriend) {
+    // Trouver l'utilisateur dans la liste des clients
+    for (int i = 0; i < numClients; ++i) {
+        if (strcmp(clients[i].username, username) == 0) {
+            // Vérifier si la personne est dans la liste d'amis
+            for (int j = 0; j < privateModeData[i].numFriends; ++j) {
+                if (strcmp(privateModeData[i].friends[j], potentialFriend) == 0) {
+                    return 1; // La personne est un ami
+                }
+            }
+            return 0; // La personne n'est pas un ami
+        }
+    }
+
+    // Gérer l'erreur si l'utilisateur n'est pas trouvé
+    printf("Error: User not found\n");
+    return 0;
+}
--- Serveur/save_game.c
+++ Serveur/save_game.c
@@ -1,1 +1,48 @@
 // Fonctions pour la sauvegarde et la relecture des parties.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "awale.h" // Inclusion du fichier qui définit la structure du plateau
+
+typedef struct {
+    AwaleBoard board;
+    // Autres informations liées à la sauvegarde de la partie
+} SavedGame;
+
+static SavedGame savedGames[MAX_CLIENTS * 2]; // Adapté à votre besoin
+
+int saveGame(const char *playerA, const char *playerB, const AwaleBoard *board) {
+    // Recherche d'un emplacement libre pour sauvegarder la partie
+    for (int i = 0; i < MAX_CLIENTS * 2; ++i) {
+        if (savedGames[i].board == NULL) {
+            // Copie du plateau de jeu
+            memcpy(&(savedGames[i].board), board, sizeof(AwaleBoard));
+            // Ajouter ici noms des joueurs, scores, classement
+            return 1; // La sauvegarde a réussi
+        }
+    }
+
+    // Gérer l'erreur si aucun emplacement libre n'est trouvé
+    printf("Error: No available slots for saving games\n");
+    return 0;
+}
+
+int loadGame(const char *playerA, const char *playerB, AwaleBoard *board) {
+    // Recherche de la partie sauvegardée
+    for (int i = 0; i < MAX_CLIENTS * 2; ++i) {
+        // Comparaison des noms des joueurs (adapté à votre besoin)
+        if (savedGames[i].board != NULL &&
+            strcmp(savedGames[i].playerA, playerA) == 0 &&
+            strcmp(savedGames[i].playerB, playerB) == 0) {
+            // Copie du plateau de jeu
+            memcpy(board, &(savedGames[i].board), sizeof(AwaleBoard));
+            return 1; // Le chargement a réussi
+        }
+    }
+
+    // Gérer l'erreur si la partie sauvegardée n'est pas trouvée
+    printf("Error: Saved game not found\n");
+    return 0;
+}
