053e6cbb6de2555514e72ead339afe8c3fc691dd
LordVERTON <daniel.verton@insa-lyon.fr>
2023-11-27 10:59:59 +0100

    structuration

---
+++ Client/chat.c
@@ -0,0 +1,8 @@
+// Fonctions pour la gestion du chat.
+// Échange de messages.
+// Bio ASCII.
+
+typedef struct {
+    // Structure de données pour gérer les messages et les bios
+} ChatData;
+
---
+++ Client/private_mode.c
@@ -0,0 +1,6 @@
+// Fonctions pour le mode privé.
+// Gestion de la liste d'amis pour les observateurs.
+
+typedef struct {
+    // Structure de données pour gérer le mode privé
+} PrivateModeData;
