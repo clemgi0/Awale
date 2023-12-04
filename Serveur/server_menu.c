76d1aeac25296bc8222606eb7b182bcf61a6ef6b
clemgi0 <clement.giraudon2@gmail.com>
2023-12-04 16:51:38 +0100

    merge 2

--- Serveur/server_menu.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <errno.h>
-
-char *getServerMenu()
-{
-    return "\n1. Get list of Clients\n2. Defy a Client\n3. Write a bio\n4. Read a bio\n";
-}
